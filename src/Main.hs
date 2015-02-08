afbf81ae8b419fc0e50765a8567d96e006dd8126
Colin King <richard.colin.king@gmail.com>
2015-02-07 18:02:21 -0600

    Stop wrapping/unwrapping File paths

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings, OverloadedLists #-}
 
 import Control.Applicative ((<$>))
-import Control.Arrow (second)
 import Control.Concurrent.Async (mapConcurrently)
 import Control.Monad.ST (runST)
 import Data.HashSet (HashSet)
@@ -17,22 +16,26 @@ import Ignore (ignore, ignored, getIgnores)
 import Types (File, Ignore, Tree(..))
 
 main :: IO ()
-main = print =<< tree i f
+main = print =<< tree i d
   where
       i = S.map ignore [".git", ".gitignore", ".hg", ".hgignore"]
-      f = (DirType 4, ".")
+      d = (DirType 4, ".")
 
 tree :: HashSet Ignore -> File -> IO (Tree RawFilePath)
-tree i (DirType 4, f) = do
-    c <- V.fromList . drop 2 <$> getDirectoryContents f
-    i' <- (`S.union` i) <$> getIgnores f
-    Folder (takeFileName f) <$>
-        (mapConcurrently (tree i') . path f . sort . V.filter (ignored i')) c
-tree _ (DirType _, f) = return (File $ takeFileName f)
+tree i (DirType 4, d) = do
+    c <- V.fromList . drop 2 <$> getDirectoryContents d
+    i' <- (`S.union` i) <$> getIgnores d
+    Folder (takeFileName d) <$>
+        (mapConcurrently (tree i') . path d . sort . V.filter (ignored i')) c
+tree _ (DirType _, f) = return (File f)
 {-# INLINABLE tree #-}
 
 path :: RawFilePath -> Vector File -> Vector File
-path f = V.map (second (f </>))
+path p = V.map step
+  where
+      step :: File -> File
+      step (DirType 4, f) = (DirType 4, p </> f)
+      step f = f
 {-# INLINABLE path #-}
 
 sort :: Vector File -> Vector File
